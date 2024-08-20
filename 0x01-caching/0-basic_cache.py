1aec6ceef5e043100ffdfd23ae39be1a81d94b5c
Abdellahsyani <syaniabdos@gmail.com>
2024-08-20 19:43:30 +0100

    basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,21 +16,3 @@ class BasicCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
-
-
-my_cache = BasicCache()
-my_cache.print_cache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-print(my_cache.get("D"))
-my_cache.print_cache()
-my_cache.put("D", "School")
-my_cache.put("E", "Battery")
-my_cache.put("A", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
